56d8190fcdb9f7a4ebd3004f683903c24a587bf2
Dong-hyeon Shin <52447545+qu3vipon@users.noreply.github.com>
2026-07-20 11:52:10 +0900

    Add files via upload

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,19 +1,13 @@
-from fastapi import FastAPI, Path, Query, Body, HTTPException
+from fastapi import FastAPI, Path, Query, Body, HTTPException, Depends
+from sqlalchemy import select
 
 from schema import UserSignUpRequest, UserUpdateRequest, UserResponse
 
-from connection import SessionFactory
+from connection import SessionFactory, get_session
 from models import User
 
 
 app = FastAPI()
-
-# 임시 데이터베이스 
-users: list[dict[str, int | str]] = [
-    {"id": 1, "name": "alex", "password": "1234"},
-    {"id": 2, "name": "bob", "password": "1234"},
-    {"id": 3, "name": "chris", "password": "1234"},
-]
 
 @app.get(
     "/users",
@@ -21,7 +15,12 @@ users: list[dict[str, int | str]] = [
     response_model=list[UserResponse],
     status_code=200
 )
-def get_users_handler():
+def get_users_handler(
+    session = Depends(get_session)
+):
+    stmt = select(User)
+    result = session.execute(stmt)
+    users: list[User] = result.scalars().all()
     return users
 
 @app.get(
@@ -31,17 +30,17 @@ def get_users_handler():
     status_code=200  # 응답이 성공한 경우, 사용할 상태코드 값 지정
 )
 def search_user_handler(
-    # GET /users/search?name=alex
-    name: str | None = Query(None)
+    name: str | None = Query(None),
+    session = Depends(get_session),
 ):
-    result = []
     if name is None:
-        return result
+        return []
 
-    for user in users:
-        if name in user["name"]:
-            result.append(user)
-    return result
+    # 검색어(name)를 name 컬럼에 포함하는 사용자 조회
+    stmt = select(User).where(User.name.contains(name))
+    result = session.execute(stmt)
+    users: list[User] = result.scalars().all()
+    return users
 
 @app.get(
     "/users/{user_id}",
@@ -50,18 +49,20 @@ def search_user_handler(
     status_code=200
 )
 def get_user_handler(
-    user_id: int = Path(..., ge=1)
+    user_id: int = Path(..., ge=1),
+    session = Depends(get_session),
 ):
-    for user in users:
-        if user_id == user["id"]:
-            return user
+    # Python 구현한 SQL 쿼리문
+    stmt = select(User).where(User.id == user_id)
+    result = session.execute(stmt)
+    user: User | None = result.scalar()
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="존재하지 않는 사용자 ID입니다."
+        )
+    return user
     
-    # user_id에 해당하는 user가 없는 경우
-    raise HTTPException(
-        status_code=404,
-        detail="존재하지 않는 사용자 ID입니다."
-    )
-
 @app.post(
     "/users",
     summary="회원 가입 API",
@@ -70,15 +71,12 @@ def get_user_handler(
 )
 def user_sign_up_handler(
     # 클라이언트가 보낸 데이터를 검사하고, 유효성 검사가 끝난 데이터
-    body: UserSignUpRequest  
+    body: UserSignUpRequest,
+    session = Depends(get_session),
 ):
     new_user = User(name=body.name, password=body.password)
-
-    session = SessionFactory()
-    session.add(new_user)
+    session.add(new_user)    
     session.commit()
-
-    session.close()
     return new_user
 
 @app.patch(
@@ -90,19 +88,25 @@ def user_sign_up_handler(
 def update_user_handler(
     user_id: int = Path(..., ge=1),
     body: UserUpdateRequest = Body(...),
+    session = Depends(get_session),
 ):
-    for user in users:
-        if user_id == user["id"]:
-            if body.name is not None:
-                user["name"] = body.name
-            if body.password is not None:
-                user["password"] = body.password
-            return user
+    stmt = select(User).where(User.id == user_id)
     
-    raise HTTPException(
-        status_code=404,
-        detail="존재하지 않는 사용자 ID입니다."
-    )
+    result = session.execute(stmt)
+    user = result.scalar()
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="존재하지 않는 사용자 ID입니다."
+        )
+
+    if body.name:
+        user.name = body.name
+    if body.password:
+        user.password = body.password
+    
+    session.commit()
+    return user
 
 @app.delete(
     "/users/{user_id}",
@@ -111,14 +115,18 @@ def update_user_handler(
     status_code=204  # NO CONTENT
 )
 def delete_user_handler(
-    user_id: int = Path(..., ge=1)
+    user_id: int = Path(..., ge=1),
+    session = Depends(get_session),
 ):
-    for user in users:
-        if user_id == user["id"]:
-            users.remove(user)
-            return
+    stmt = select(User).where(User.id == user_id)
     
-    raise HTTPException(
-        status_code=404,
-        detail="존재하지 않는 사용자 ID입니다."
-    )
+    result = session.execute(stmt)
+    user = result.scalar()   
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="존재하지 않는 사용자 ID입니다."
+        )
+    
+    session.delete(user)  # 삭제 마킹
+    session.commit()
